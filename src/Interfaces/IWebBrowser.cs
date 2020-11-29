05faa9e1858523f5fed956572d4a2190b2544293
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2020-11-29 16:41:03 +0100

    Speed up build time by de-referencing MSHTML, it is not really needed here

--- src/Interfaces/IWebBrowser.cs
+++ src/Interfaces/IWebBrowser.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using MSHTML;
 
 namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Interfaces {
     public interface IWebBrowser {
         string Url { get; set; }
         bool IsNavigating { get; set; }
-        IHTMLDocument3 Document { get; set; }
         DateTime LastNavigationStartedAt { get; set; }
         string AskedForNavigationToUrl { get; set; }
     }

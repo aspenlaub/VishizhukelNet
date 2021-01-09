6e6e25dbb0da338dc67f83d247a6a9157f68f8da
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-01-09 16:16:25 +0100

    Move tash communicator from Roust to here

---
+++ src/Entities/TashTaskHandlingStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Aspenlaub.Net.GitHub.CSharp.Tash;
+using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Interfaces;
+
+namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Entities {
+    public class TashTaskHandlingStatus<TModel> : ITashTaskHandlingStatus<TModel> where TModel : IApplicationModel {
+        public TModel Model { get; }
+        public int ProcessId { get; }
+        public DateTime StatusLastConfirmedAt { get; set; }
+        public List<ControllableProcessTask> ControllableProcessTasks { get; set; }
+        public ControllableProcessTask TaskBeingProcessed { get; set; }
+
+        public TashTaskHandlingStatus(TModel model, int processId) {
+            Model = model;
+            ProcessId = processId;
+            StatusLastConfirmedAt = DateTime.MinValue;
+            ControllableProcessTasks = new List<ControllableProcessTask>();
+            TaskBeingProcessed = null;
+        }
+    }
+}
---
+++ src/Handlers/TashCommunicatorBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Aspenlaub.Net.GitHub.CSharp.Tash;
+using Aspenlaub.Net.GitHub.CSharp.TashClient.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Application;
+using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Interfaces;
+
+namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Handlers {
+    public class TashCommunicatorBase<TModel> : ITashCommunicator<TModel> where TModel : IApplicationModel {
+        private readonly ITashAccessor vTashAccessor;
+        private readonly IApplicationLogger vApplicationLogger;
+
+        public TashCommunicatorBase(ITashAccessor tashAccessor, IApplicationLogger applicationLogger) {
+            vTashAccessor = tashAccessor;
+            vApplicationLogger = applicationLogger;
+        }
+
+        public async Task CommunicateAndShowCompletedOrFailedAsync(ITashTaskHandlingStatus<TModel> status, bool setText, string text) {
+            vApplicationLogger.LogMessage("Communicating 'Completed' to remote controlling process");
+            await ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.Completed, setText, text, "");
+        }
+
+        public async Task ChangeCommunicateAndShowProcessTaskStatusAsync(ITashTaskHandlingStatus<TModel> status,
+            ControllableProcessTaskStatus newStatus) {
+            await ChangeCommunicateAndShowProcessTaskStatusAsync(status, newStatus, false, "", "");
+        }
+
+        public async Task ChangeCommunicateAndShowProcessTaskStatusAsync(ITashTaskHandlingStatus<TModel> status,
+            ControllableProcessTaskStatus newStatus, bool setText, string text, string errorMessage) {
+            status.TaskBeingProcessed.Status = newStatus;
+            if (newStatus == ControllableProcessTaskStatus.Failed || newStatus == ControllableProcessTaskStatus.BadRequest) {
+                status.TaskBeingProcessed.ErrorMessage = errorMessage;
+            }
+
+            if (setText) {
+                status.TaskBeingProcessed.Text = text;
+            }
+
+            await vTashAccessor.ConfirmStatusAsync(status.TaskBeingProcessed.Id, status.TaskBeingProcessed.Status, status.TaskBeingProcessed.Text, status.TaskBeingProcessed.ErrorMessage);
+            await ConfirmAliveAsync(status, ControllableProcessStatus.Idle, DateTime.Now);
+            await ShowStatusAsync(status);
+        }
+
+        public async Task<HttpStatusCode> ConfirmAliveAsync(ITashTaskHandlingStatus<TModel> status, ControllableProcessStatus cpStatus, DateTime time) {
+            return await vTashAccessor.ConfirmAliveAsync(status.ProcessId, time, cpStatus);
+        }
+
+        public async Task ShowStatusAsync(ITashTaskHandlingStatus<TModel> status) {
+            await Task.Run(() => { });
+        }
+    }
+}

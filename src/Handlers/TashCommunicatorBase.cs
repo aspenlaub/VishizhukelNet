a4e7431b8e9ac0ddc78feb69e6617dab789ec964
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-01-10 18:46:26 +0100

    Temporary fix

--- src/Handlers/TashCommunicatorBase.cs
+++ src/Handlers/TashCommunicatorBase.cs
@@ -42,7 +42,14 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Handlers {
                 status.TaskBeingProcessed.Text = text;
             }
 
-            await TashAccessor.ConfirmStatusAsync(status.TaskBeingProcessed.Id, status.TaskBeingProcessed.Status, status.TaskBeingProcessed.Text, status.TaskBeingProcessed.ErrorMessage);
+            // TODO: reduce to one attempt
+            var attempts = 10;
+            HttpStatusCode statusCode;
+            do {
+                statusCode = await TashAccessor.ConfirmStatusAsync(status.TaskBeingProcessed.Id, status.TaskBeingProcessed.Status,
+                    status.TaskBeingProcessed.Text, status.TaskBeingProcessed.ErrorMessage);
+            } while (statusCode == HttpStatusCode.NotFound && --attempts >= 0);
+
             await ConfirmAliveAsync(status, ControllableProcessStatus.Idle, DateTime.Now);
             await ShowStatusAsync(status);
         }

bf3712149806dd611ba4180d96e1266c488d5c15
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-01-27 11:01:08 +0100

    Use new integration test tools package

--- src/Integration/Test/DemoWindowUnderTestActions.cs
+++ src/Integration/Test/DemoWindowUnderTestActions.cs
@@ -1,49 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Aspenlaub.Net.GitHub.CSharp.Pegh.Helpers;
 using Aspenlaub.Net.GitHub.CSharp.Tash;
 using Aspenlaub.Net.GitHub.CSharp.TashClient.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test.DemoApplication.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.VishnetIntegrationTestTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using ControllableProcessTaskType = Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Entities.ControllableProcessTaskType;
 
 namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Integration.Test {
-    public class DemoWindowUnderTestActions {
-        private readonly ITashAccessor vTashAccessor;
-        private bool vInitialized;
-
-        public DemoWindowUnderTestActions(ITashAccessor tashAccessor) {
-            vTashAccessor = tashAccessor;
-            vInitialized = false;
-        }
-
-        public virtual async Task InitializeAsync() {
-            var errorsAndInfos = await vTashAccessor.EnsureTashAppIsRunningAsync();
-            Assert.IsFalse(errorsAndInfos.AnyErrors(), string.Join("\r\n", errorsAndInfos.Errors));
-
-            await vTashAccessor.AssumeDeath(p => p.Title == "Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test");
-
-            var processes = await vTashAccessor.GetControllableProcessesAsync();
-            Assert.IsFalse(processes.Any(p => p.Title == "Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test" && p.Status != ControllableProcessStatus.Dead), "Non-dead processes exist before test execution");
-            vInitialized = true;
-        }
-
-        public async Task<ControllableProcess> FindIdleProcessAsync() {
-            if (!vInitialized) { throw new Exception("InitializeAsync has not been called"); }
-
-            ControllableProcess process = null;
-            await Wait.UntilAsync(async () => (process = await TryFindIdleProcess()) != null, TimeSpan.FromSeconds(30));
-            Assert.IsNotNull(process);
-            return process;
-        }
-
-        protected async Task<ControllableProcess> TryFindIdleProcess() {
-            var findIdleProcessResult = await vTashAccessor.FindIdleProcess(p => p.Title == "Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test");
-            return findIdleProcessResult.ControllableProcess;
+    public class DemoWindowUnderTestActions : WindowUnderTestActionsBase {
+        public DemoWindowUnderTestActions(ITashAccessor tashAccessor) : base(tashAccessor, "Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test") {
         }
 
         public ControllableProcessTask CreateResetTask(ControllableProcess process) {
@@ -71,11 +40,11 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Integration.Test {
         }
 
         public async Task<string> SubmitNewTaskAndAwaitCompletionAsync(ControllableProcessTask task, bool successIsExpected) {
-            var status = await vTashAccessor.PutControllableProcessTaskAsync(task);
+            var status = await TashAccessor.PutControllableProcessTaskAsync(task);
             Assert.AreEqual(HttpStatusCode.Created, status);
 
-            await vTashAccessor.AwaitCompletionAsync(task.Id, 20000);
-            var result = vTashAccessor.GetControllableProcessTaskAsync(task.Id).Result;
+            await TashAccessor.AwaitCompletionAsync(task.Id, 20000);
+            var result = TashAccessor.GetControllableProcessTaskAsync(task.Id).Result;
             if (successIsExpected) {
                 var errorMessage = $"Task status is {Enum.GetName(typeof(ControllableProcessTaskStatus), result.Status)}, error message: {result.ErrorMessage}";
                 Assert.AreEqual(ControllableProcessTaskStatus.Completed, result.Status, errorMessage);

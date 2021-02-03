7d97f049170ff1dfbc7c10cc5e5d37ce85e40344
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-02-03 07:32:38 +0100

    Consolidate duplicated code

--- src/Handlers/TashVerifyAndSetHandlerBase.cs
+++ src/Handlers/TashVerifyAndSetHandlerBase.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Tash;
+using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Controls;
+using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Enums;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -21,8 +24,6 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Handlers {
             Selectors = selectors;
         }
 
-        public abstract Task ProcessVerifyWhetherEnabledTaskAsync(ITashTaskHandlingStatus<TModel> status);
-        public abstract Task ProcessVerifyNumberOfItemsTaskAsync(ITashTaskHandlingStatus<TModel> status);
         protected abstract Dictionary<string, ITextBox> TextBoxNamesToTextBoxDictionary(ITashTaskHandlingStatus<TModel> status);
         protected abstract Dictionary<string, ISimpleTextHandler> TextBoxNamesToTextHandlerDictionary(ITashTaskHandlingStatus<TModel> status);
 
@@ -93,6 +94,79 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Handlers {
                 await textHandler.TextChangedAsync(text);
             }
             return label ? textBox.LabelText : textBox.Text;
+        }
+
+        public async Task ProcessVerifyWhetherEnabledTaskAsync(ITashTaskHandlingStatus<TModel> status) {
+            bool actualEnabled;
+            var properties = typeof(TModel).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var property = properties.FirstOrDefault(p => p.Name == status.TaskBeingProcessed.ControlName);
+            if (property == null) {
+                var errorMessage = $"Unknown enabled/disabled control {status.TaskBeingProcessed.ControlName}";
+                SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process ({errorMessage})");
+                await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
+                return;
+            }
+
+            var propertyValue = property.GetValue(status.Model);
+            switch (propertyValue) {
+                case Button button:
+                    actualEnabled = button.Enabled;
+                    break;
+                case ISelector selector:
+                    actualEnabled = selector.Selectables.Any();
+                    break;
+                case TextBox textBox:
+                    actualEnabled = textBox.Enabled;
+                    break;
+                case ToggleButton button:
+                    actualEnabled = button.Enabled;
+                    break;
+                default: {
+                    var errorMessage = $"Unknown enabled/disabled control {status.TaskBeingProcessed.ControlName}";
+                    SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process ({errorMessage})");
+                    await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
+                    return;
+                }
+            }
+
+            if (status.TaskBeingProcessed.Text == "true") {
+                if (actualEnabled) {
+                    status.Model.Status.Type = StatusType.Success;
+                    status.Model.Status.Text = "";
+                } else {
+                    status.Model.Status.Type = StatusType.Error;
+                    status.Model.Status.Text = $"Expected {status.TaskBeingProcessed.ControlName} to be enabled";
+                }
+            } else if (actualEnabled) {
+                status.Model.Status.Type = StatusType.Error;
+                status.Model.Status.Text = $"Expected {status.TaskBeingProcessed.ControlName} to be disabled";
+            } else {
+                status.Model.Status.Type = StatusType.Success;
+                status.Model.Status.Text = "";
+            }
+            await TashCommunicator.CommunicateAndShowCompletedOrFailedAsync(status, false, "");
+        }
+
+        public async Task ProcessVerifyNumberOfItemsTaskAsync(ITashTaskHandlingStatus<TModel> status) {
+            if (string.IsNullOrWhiteSpace(status.TaskBeingProcessed.Text)) {
+                var errorMessage = $"No number of items specified for {status.TaskBeingProcessed.ControlName}";
+                SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process {errorMessage}");
+                await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
+                return;
+            }
+
+            if (!Selectors.ContainsKey(status.TaskBeingProcessed.ControlName)) {
+                var errorMessage = $"Unknown control {status.TaskBeingProcessed.ControlName} with number of items";
+                SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process ({errorMessage})");
+                await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
+                return;
+            }
+            var actualNumberOfItems = Selectors[status.TaskBeingProcessed.ControlName].Selectables.Count;
+            status.Model.Status.Text = actualNumberOfItems.ToString() == status.TaskBeingProcessed.Text
+                ? ""
+                : $"Expected {status.TaskBeingProcessed.Text} item/-s on {status.TaskBeingProcessed.ControlName}, got {actualNumberOfItems}";
+            status.Model.Status.Type = string.IsNullOrEmpty(status.Model.Status.Text) ? StatusType.Success : StatusType.Error;
+            await TashCommunicator.CommunicateAndShowCompletedOrFailedAsync(status, false, "");
         }
     }
 }
--- src/Test/DemoApplication/Handlers/TashVerifyAndSetHandler.cs
+++ src/Test/DemoApplication/Handlers/TashVerifyAndSetHandler.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
-using Aspenlaub.Net.GitHub.CSharp.Tash;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Enums;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Handlers;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test.DemoApplication.Interfaces;
-using Microsoft.Extensions.Logging;
 
 namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test.DemoApplication.Handlers {
     public class TashVerifyAndSetHandler : TashVerifyAndSetHandlerBase<IDemoApplicationModel> {
@@ -17,58 +13,6 @@ namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test.DemoApplication.Handle
             ITashCommunicator<IDemoApplicationModel> tashCommunicator, Dictionary<string, ISelector> selectors)
             : base(simpleLogger, tashSelectorHandler, tashCommunicator, selectors) {
             vDemoApplicationHandlers = demoApplicationHandlers;
-        }
-
-        public override async Task ProcessVerifyWhetherEnabledTaskAsync(ITashTaskHandlingStatus<IDemoApplicationModel> status) {
-            bool actualEnabled;
-            switch (status.TaskBeingProcessed.ControlName) {
-                case nameof(status.Model.Alpha):
-                    actualEnabled = status.Model.Alpha.Enabled;
-                    break;
-                case nameof(status.Model.Beta):
-                    actualEnabled = status.Model.Beta.Selectables.ToList().Any();
-                    break;
-                default:
-                    var errorMessage = $"Unknown enabled/disabled control {status.TaskBeingProcessed.ControlName}";
-                    SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process ({errorMessage})");
-                    await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
-                    return;
-            }
-
-            status.Model.Status.Type = StatusType.Success;
-            if (status.TaskBeingProcessed.Text == "true") {
-                if (!actualEnabled) {
-                    status.Model.Status.Type = StatusType.Error;
-                    status.Model.Status.Text = $"Expected {status.TaskBeingProcessed.ControlName} to be enabled";
-                }
-            } else if (actualEnabled) {
-                status.Model.Status.Type = StatusType.Error;
-                status.Model.Status.Text = $"Expected {status.TaskBeingProcessed.ControlName} to be disabled";
-            }
-
-            await TashCommunicator.CommunicateAndShowCompletedOrFailedAsync(status, false, "");
-        }
-
-        public override async Task ProcessVerifyNumberOfItemsTaskAsync(ITashTaskHandlingStatus<IDemoApplicationModel> status) {
-            if (string.IsNullOrWhiteSpace(status.TaskBeingProcessed.Text)) {
-                var errorMessage = $"No number of items specified for {status.TaskBeingProcessed.ControlName}";
-                SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process {errorMessage}");
-                await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
-                return;
-            }
-
-            if (!Selectors.ContainsKey(status.TaskBeingProcessed.ControlName)) {
-                var errorMessage = $"Unknown control {status.TaskBeingProcessed.ControlName} with number of items";
-                SimpleLogger.LogInformation($"Communicating 'BadRequest' to remote controlling process ({errorMessage})");
-                await TashCommunicator.ChangeCommunicateAndShowProcessTaskStatusAsync(status, ControllableProcessTaskStatus.BadRequest, false, "", errorMessage);
-                return;
-            }
-            var actualNumberOfItems = Selectors[status.TaskBeingProcessed.ControlName].Selectables.Count;
-            status.Model.Status.Text = actualNumberOfItems.ToString() == status.TaskBeingProcessed.Text
-                ? ""
-                : $"Expected {status.TaskBeingProcessed.Text} item/-s on {status.TaskBeingProcessed.ControlName}, got {actualNumberOfItems}";
-            status.Model.Status.Type = string.IsNullOrEmpty(status.Model.Status.Text) ? StatusType.Success : StatusType.Error;
-            await TashCommunicator.CommunicateAndShowCompletedOrFailedAsync(status, false, "");
         }
 
         protected override Dictionary<string, ITextBox> TextBoxNamesToTextBoxDictionary(ITashTaskHandlingStatus<IDemoApplicationModel> status) {

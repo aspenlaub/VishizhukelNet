594f4d0c84b9a5eb1c12e628f53937e40c9c93bb
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-04-23 10:52:56 +0200

    Vishizhukel update

---
+++ src/Test/Miscellaneous/ContainerBuilderTest.cs
@@ -0,0 +1,19 @@
+﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
+using Aspenlaub.Net.GitHub.CSharp.TashClient.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Web;
+using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Aspenlaub.Net.GitHub.CSharp.VishizhukelNet.Test.Miscellaneous {
+    [TestClass]
+    public class ContainerBuilderTest {
+        [TestMethod]
+        public void CanUseContainerBuilder() {
+            var logConfigurationMock = new Mock<ILogConfiguration>();
+            var container = new ContainerBuilder().UseVishizhukelNetDvinAndPegh(new DummyCsArgumentPrompter(), logConfigurationMock.Object).Build();
+            var httpGate = container.Resolve<ISecuredHttpGate>();
+            Assert.IsNotNull(httpGate);
+        }
+    }
+}
